97044816d1a4beb3ea76527d41ee691e98e7fa43
JenyferMarquez <you@example.com>
2022-03-07 10:20:00 -0500

    conectando firebase

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './firebase/firebaseConfig';
+
+function App() {
+  useEffect(() => {
+    const obtenerDatos = async () => {
+      const datos = await getDocs(collection(db, 'usuarios'));
+      datos.forEach((documento) => {
+        console.log(documento.data());
+      });
+    };
+    obtenerDatos();
+  }, []);
+  return (
+    <h1>firebase</h1>
+  );
+}
+export default App;
